b2fd9558a14ebc30cf1280821448951d6f3c3d0e
MatheusAprigio <matheusaprig@Gmail.com>
2020-06-21 22:56:08 -0300

    Modified index CSS

--- src/main/java/com/gitapi/controllers/MainController.java
+++ src/main/java/com/gitapi/controllers/MainController.java
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class IndexController {
+public class MainController {
 
     @Autowired
     UserService userService;
